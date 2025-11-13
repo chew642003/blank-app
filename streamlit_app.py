c9d3a96c8fac9f8a183f83fa87e255c8e7a8edae
chew642003 <155594344+chew642003@users.noreply.github.com>
2025-11-13 01:28:45 +0000

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   README.md
    	modified:   requirements.txt
    	modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
+import clips
+import logging
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+logging.basicConfig(level=15,format='%(message)s')
+
+env = clips.Environment()
+router = clips.loggingRouter()
+env.add_router(router)
+
+name = st.text_input("Enter your name")
+
+env.build("(deftemplate result (slot name))")
+env.assert_string(f'(result(name "{name}"))')
+env.run()
+
+results = []
+for fact in env.facts():
+    if fact.template.name == 'result' :
+        results.append(fact['name'])
+
+st.write(results[0],"output")
